c9251d64e5df52a6799c1e54f8a45769abe33297
Faheem Kamal <faheem.kamal.a@gmail.com>
2018-02-05 11:50:26 +0600

    Create netsh start.cmd

---
+++ netsh start.cmd
@@ -0,0 +1,13 @@
+@echo off
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit)
+
+:loop
+@echo off
+netsh wlan stop hostednetwork
+netsh interface set interface name="WiFi" admin=DISABLED
+netsh interface set interface name="WiFi" admin=ENABLED
+netsh wlan set hostednetwork mode=allow ssid=faheemk key=12345678
+netsh wlan start hostednetwork
+@pause
+
+goto loop
