fcd97e93c1c4c7d8124180a19efb88b02e8bf0f9
Faheem Kamal <faheem.kamal.a@gmail.com>
2018-02-06 16:11:34 +0600

    restart ICS

--- netsh start.cmd
+++ netsh start.cmd
@@ -4,6 +4,8 @@ if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit)
 :loop
 @echo off
 netsh wlan stop hostednetwork
+net stop sharedaccess
+net start sharedaccess
 netsh interface set interface name="WiFi" admin=DISABLED
 netsh interface set interface name="WiFi" admin=ENABLED
 netsh wlan set hostednetwork mode=allow ssid=faheemk key=12345678
