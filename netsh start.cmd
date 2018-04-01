3162d73d4540a414cadea80514d313154f3e8a1c
Faheem Kamal <faheem.kamal.a@gmail.com>
2018-04-01 10:53:35 +0600

    Update netsh start.cmd

--- netsh start.cmd
+++ netsh start.cmd
@@ -4,10 +4,10 @@ if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit)
 :loop
 @echo off
 netsh wlan stop hostednetwork
-#net stop sharedaccess
-#net start sharedaccess
-#netsh interface set interface name="WiFi" admin=DISABLED
-#netsh interface set interface name="WiFi" admin=ENABLED
+REM net stop sharedaccess
+REM net start sharedaccess
+netsh interface set interface name="Wi-Fi" admin=DISABLED
+netsh interface set interface name="Wi-Fi" admin=ENABLED
 netsh wlan set hostednetwork mode=allow ssid=faheemk key=12345678
 netsh wlan start hostednetwork
 @pause
